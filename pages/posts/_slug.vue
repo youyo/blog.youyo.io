40036f32f96cf9ab146f53cf0d85af771c3042d0
youyo <1003ni2@gmail.com>
2019-05-07 03:05:51 +0900

    update

--- pages/posts/_slug.vue
+++ pages/posts/_slug.vue
@@ -1,14 +1,14 @@
 <template>
   <v-container grid-list-xs>
     <v-layout justify-center row wrap>
-      <v-flex xs10 class="ma-4">
+      <v-flex xs11 md10 text-xs-center>
         <span class="display-1 font-weight-bold">{{post.fields.title}}</span>
       </v-flex>
-      <v-flex xs12 class="ma-4">
+      <v-flex xs12 class="my-4">
         <hr>
       </v-flex>
-      <v-flex xs9 class="ma-3" subheading>
-        <div subheading v-html="$md.render(post.fields.body)"></div>
+      <v-flex xs12 sm11 md9 class="mt-2 mb-5" subheading>
+        <div v-html="$md.render(post.fields.body)"></div>
       </v-flex>
     </v-layout>
   </v-container>
